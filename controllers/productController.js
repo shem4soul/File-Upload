3ef06166fe5f9210df6034e9b3c4e8daea24d0b0
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-18 18:16:46 +0100

    Implement createProduct function to handle product creation and return created product data

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,10 @@ const Product = require('../models/Product')
 const {StatusCodes} = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-    return res.send('create product')
+  console.log(req.body);
+  const product = await Product.create(req.body)
+   res.status(StatusCodes.CREATED).json({product})
+
 }
 const getAllProducts = async (req, res) => {
   return res.send('list of products')
