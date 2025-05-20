e536a451d359d1352578737fe8a7f7ef0e94a115
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-20 10:40:30 +0100

    Refactor uploadProductImage function to handle image upload and improve path construction

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,12 +1,19 @@
-const Product = require("../models/Product");
+const path = require("path");
+
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
-  console.log(req);
-  
-  res.send("upload product image");
-};
+  let productImage = req.files.image;
 
-module.exports = {
- uploadProductImage
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads",
+    +`${productImage.name}`
+  );
+
+  await productImage.mv(imagePath)
+  res.send('upload product image');
 }
+module.exports = {
+  uploadProductImage,
+};
