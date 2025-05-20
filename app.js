d3cf41e71332012ea03cf740a544aadb86561dac
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-20 20:27:23 +0100

    configure cloudinary

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const express = require('express');
 const app = express();
 
 const fileUpload = require('express-fileupload');
-
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // database
 const connectDB = require('./db/connect');
@@ -20,7 +25,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.static('./public'));
 app.use(express.json());
-app.use(fileUpload());
+app.use(fileUpload({useTempFiles: true, tempFileDir: './tmp'}));
 
 
 
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,12 @@
 const path = require("path");
+const fs = require("fs");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 
-const uploadProductImage = async (req, res) => {
+const cloudinary = require("cloudinary").v2;
+
+const uploadProductImageLocal = async (req, res) => {
  
   console.log(req.files);
   if (!req.files) {
@@ -34,6 +37,63 @@ const uploadProductImage = async (req, res) => {
     .json({ image: { src: `/uploads/${productImage.name}` } })
    
 }
+// const uploadProductImage = async (req, res) => {
+//   const result = await cloudinary.uploader.upload(
+//     req.files.image.tempFilePath,
+//     {
+//       use_filename: true,
+//       folder: "file-upload",
+//     }
+//   );
+//   console.log(result);
+//   // res.status(200).json({ image: { src: result.secure_url } });
+// };
+
+
+
+
+const uploadProductImage = async (req, res) => {
+  try {
+    // 1) Validation
+    if (!req.files?.image) {
+      throw new CustomError.BadRequestError("No file uploaded");
+    }
+    const productImage = req.files.image;
+    if (!productImage.mimetype.startsWith("image")) {
+      throw new CustomError.BadRequestError("Please upload an image");
+    }
+    if (!productImage.tempFilePath) {
+      throw new CustomError.BadRequestError("Temp file path is missing");
+    }
+    if (productImage.size > 1 * 1024 * 1024) {
+      throw new CustomError.BadRequestError("Image must be smaller than 1MB");
+    }
+
+    // 2) Unsigned upload with folder override
+    const result = await cloudinary.uploader.unsigned_upload(
+      productImage.tempFilePath,
+      "unsigned_images", // your preset name
+      { folder: "file-upload" } // override the folder here
+    );
+
+    // 3) Cleanup temp file
+    // fs.unlinkSync(productImage.tempFilePath);
+
+    // 4) Respond
+    return res.status(StatusCodes.OK).json({
+      image: {
+        src: result.secure_url,
+        public_id: result.public_id,
+      },
+    });
+  } catch (error) {
+    console.error("Cloudinary Upload Error:", error);
+    return res
+      .status(error.http_code || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: error.message });
+  }
+};
+
 module.exports = {
   uploadProductImage,
 };
