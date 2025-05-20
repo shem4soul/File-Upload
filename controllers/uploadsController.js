f26fbd9c6d3581f84b86fbb9b34bef996d64f2cc
shem4soul@gmail.com <shem4soul@gmail.com>
2025-05-20 17:07:41 +0100

    Add validation checks for uploaded product images in uploadProductImage function

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,10 +1,27 @@
 const path = require("path");
-
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 
 const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+ 
+  console.log(req.files);
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  
+  }
 
+   const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload an image");  
+  }
+
+  const maxSize = 1024 * 1024; // 1MB
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      `Please upload an image smaller than ${maxSize / 1024} KB`
+    );
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads",
